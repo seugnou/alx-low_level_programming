efcc20295868104a1a72bb37993ce38bc95038ad
root <claudiaseugnou@gmail.com>
2022-03-11 17:33:19 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,19 +14,22 @@ int main(void)
 
 	for (digit1 = '0'; digit1 < '9'; digit1++)
 	{
-		for (digit3 = digit2 + 1; digit3 <= '9'; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
 		{
-			if ((digit2 != digit1) != digit3)
+			for (digit3 = digit2 + 1; digit3 <= '9'; digit3++)
 			{
-				putchar(digit1);
-				putchar(digit2);
-				putchar(digit3);
+				if ((digit2 != digit1) != digit3)
+				{
+					putchar(digit1);
+					putchar(digit2);
+					putchar(digit3);
 
-				if (digit1 == '7' && digit2 == '8')
-					continue;
+					if (digit1 == '7' && digit2 == '8')
+						continue;
 
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
