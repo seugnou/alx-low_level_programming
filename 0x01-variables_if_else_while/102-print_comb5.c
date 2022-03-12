05c33bb8b0d00976c9e134b98da1113481478f8a
root <claudiaseugnou@gmail.com>
2022-03-11 16:46:43 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**main - Entry point
+/**
+*main - Entry point
 *
 *Return: Always 0 (Success)
 */
@@ -10,25 +11,25 @@ int main(void)
 {
 	int Num1, Num2;
 
-	for (Num1 = 0; Num1<=98; Num1++)
+	for (Num1 = 0; Num1 <= 98; Num1++)
 	{
-		for (Num2 = Num1 + 1; Num2 <= 99; Num++)
-		{
-			putchar((Num1 / 10) + '0');
-			putchar((Num1 % 10) + '0');
-			putchar(' ');
-			putchar((Num2 / 10) + '0');
-			putchar((Num2 % 10) + '0');
+	for (Num2 = Num1 + 1; Num2 <= 99; Num++)
+	{
+	putchar((Num1 / 10) + '0');
+	putchar((Num1 % 10) + '0');
+	putchar(' ');
+	putchar((Num2 / 10) + '0');
+	putchar((Num2 % 10) + '0');
 
-			if (Num1 == 98 && Num2 == 99)
-			       continue;
+	if (Num1 == 98 && Num2 == 99)
 
-		        putchar(',');
-		        putchar(',');
-		}
+	continue;
+
+	putchar(',');
+	putchar(' ');
 	}
-        
-        putchar('\n');
- 
-        return(0);
-}	
+	}
+	putchar('\n');
+
+	return (0);
+}
