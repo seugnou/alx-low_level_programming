a96948f878a50774eb8cbdf035a0c140380c3df1
root <claudiaseugnou@gmail.com>
2022-03-14 16:45:17 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char lowc;
+
+	for (lowc = 0; lowc <= 16; lowc++)
+		putchar("%x", lowc);
+	putchar('\n);
+
+	return (0);
+
+}
