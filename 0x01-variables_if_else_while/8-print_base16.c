d6f8066636329258564eb04b6d128c8b97a0b956
root <claudiaseugnou@gmail.com>
2022-03-14 18:01:24 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,11 +13,9 @@ int main(void)
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
-
-			putchar(num);
+		putchar(num);
 
 	for (lowc = 'a'; lowc <= 'f'; lowc++)
-
 		putchar(lowc);
 
 	putchar('\n');
