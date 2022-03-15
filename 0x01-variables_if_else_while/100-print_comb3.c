10a2b69b97141cd07bf2170956b0a9b375b20f4d
root <claudiaseugnou@gmail.com>
2022-03-14 19:51:31 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,16 +18,16 @@ int main(void)
 		for (d2 = d1 + 1; d2 <= '9'; d2++)
 		{
 			if (d2 != d1)
-		{
-			putchar(d1);
-			putchar(d2);
+			{
+				putchar(d1);
+				putchar(d2);
 
-			if (d1 == '8' && d2 == '9')
+				if (d1 == '8' && d2 == '9')
 				continue;
 
 				putchar(',');
 				putchar(' ');
-		}
+			}
 		}
 	}
 	putchar('\n');
