721e18534685a9fd9621f1b8bf6788835706f3c3
root <claudiaseugnou@gmail.com>
2022-03-14 19:47:46 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = '0'; d1 <= '9'; d1++)
+	{
+		for (d2 = d1 + 1; d2 <= '9'; d2++)
+		{
+			if (d2 != d1)
+		{
+			putchar(d1);
+			putchar(d2);
+
+			if (d1 == '8' && d2 == '9')
+				continue;
+
+				putchar(',');
+				putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
