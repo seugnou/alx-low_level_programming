c02605e7572fec27f0d031ec6a630d2e912a0a74
root <claudiaseugnou@gmail.com>
2022-03-16 18:05:02 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,17 +11,17 @@
 
 int _islower(int c)
 {
-	int c;
+	int r;
 
-	c = _islower('H');
+	r = _islower('H');
 
-	_putchar(c + '0');
+	_putchar(r + '0');
 
-	c = _islower('o');
-	_putchar(c + '0');
+	r = _islower('o');
+	_putchar(r + '0');
 
-	c = _islower('108');
-	_putchar(c +'0');
+	r = _islower('108');
+	_putchar(r +'0');
 
 	_putchar('\n');
 
