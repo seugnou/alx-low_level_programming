20312465adbe93850a4aac86050982ceed29df80
root <claudiaseugnou@gmail.com>
2022-03-17 15:49:41 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%d\n", mull(98, 1024));
+	printf("%d\n", mull(-402, 4096));
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - it's a function that multiplies two integers
+ * @a: An input integer
+ * @b: An input integer
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
