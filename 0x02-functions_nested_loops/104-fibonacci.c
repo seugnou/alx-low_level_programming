7008eae68e33a7bfaa0cf852831d2737a09e00c4
root <claudiaseugnou@gmail.com>
2022-03-16 18:43:19 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,11 +13,11 @@ int main(void)
 	long int fibonacci[98], sum = 2;
 
 	fibonacci[0] = 1;
-	fibonnacci[1] = 2;
+	fibonacci[1] = 2;
 
 	for (i = 2; i < 98; ++i)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonnacci[i - 2];
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if ((fibonacci[i] % 2 == 0 && fibonacci[i] < 500000000000)
 					sum += fibonacci[i];
 	}
