15d58f5236b779a3871ed8d0f7f6695a9daa6cdf
root <claudiaseugnou@gmail.com>
2022-03-16 18:37:03 -0700

    Task 104

---
+++ 0x02-functions_nested_loops/104_fibonacci.c
@@ -0,0 +1,27 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	int i;
+	long int fibonacci[98], sum = 2;
+
+	fibonacci[0] = 1;
+	fibonnacci[1] = 2;
+
+	for (i = 2; i < 98; ++i)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonnacci[i - 2];
+		if ((fibonacci[i] % 2 == 0 && fibonacci[i] < 500000000000)
+					sum += fibonacci[i];
+	}
+	 printf("%ld\n", sum);
+
+	 return (0);
+}
