0c6f832e0f8b6e26a1ea208f238b78eceee4ba4b
root <claudiaseugnou@gmail.com>
2022-03-16 17:43:25 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check code
+ * Description: this code prints the alphabets in lowercase 10 times
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	char lowcLine1;
+	int lowcInc;
+
+	for (lowcInc = 0; lowcInc < 10; ++lowcInc)
+	{
+		for (lowc = 'a'; lowc <= 'z'; ++lowc)
+			_putchar(lowc);
+		_putchar('\n');
+
+	}
+}
