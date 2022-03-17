b4fa96e27969df9e882fb8aa82da3b02421bf402
root <claudiaseugnou@gmail.com>
2022-03-16 17:22:45 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+void print_alphabet(void)
+{
+	char lowc;
+
+	for (lowc = 'a'; lowc <= 'z'; ++lowc)
+		_putchar(lowc);
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,30 @@
 
 /*_putchar.c */
 int _putchar(char);
+
+/*1x_alphabet*/
+void print_alphabet(void);
+
+/*alphabet_x10*/
+void print_alphabet_x10(void);
+
+/*3_is_lower*/
+int _islower(int c);
+
+/*4_is_alpha*/
+int _isalpha(int c);
+
+/*5_sign*/
+int print_sign(int r);
+
+/*6_abs*/
+int _abs(int r);
+
+/*7_print_last_digit*/
+int print_last_digit(int r);
+
+/*8_24_hours*/
+void jack_bauer(void);
+
+/*9_times_tables*/
+void times_tables(void);
