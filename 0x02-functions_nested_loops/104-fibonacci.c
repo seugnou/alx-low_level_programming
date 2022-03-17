826e09a6353b59e26d1e2b76aa44b837b91a3064
root <claudiaseugnou@gmail.com>
2022-03-16 19:47:55 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,13 +18,13 @@ int main(void)
 	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
-	for (i = 1; i <=98 ; ++i)
+	for (i = 2; i <= 98 ; ++i)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 
 		if (i == 98)
 
-	 	printf("%ld\n", fibonacci[i]);
+	 		printf("%ld\n", fibonacci[i]);
 		else
 			printf("%ld, ", fibonacci[i]);
 	}
