8267df65609228dde474137cb71c27845b8a993b
root <claudiaseugnou@gmail.com>
2022-03-18 15:16:31 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,14 @@
 
 void print_most_numbers(void)
 {
-	int i = '0';
+	int i = 0;
 
 	for (; i < 10; i++)
 	{
-		if (i != '2' && i != '4')
+		if (i != 2 && i != 4)
+		{
 			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
