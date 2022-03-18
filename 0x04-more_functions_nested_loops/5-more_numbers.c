aa3ab493fd1b524348bc95dbca8837f23c559fab
root <claudiaseugnou@gmail.com>
2022-03-17 19:32:00 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,16 @@ void more_numbers(void)
 	for (j =0; j < 10; j++)
 	{
 	
-		for (i = 0; i <=14; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			if (i>9)
+			if (i >= 10 )
+			{
 				_putchar(i / 10 + '0');
+			}
 
 			_putchar(i % 10 + '0');
+		i++;
+
 		}
 	_putchar('\n');
 	}
