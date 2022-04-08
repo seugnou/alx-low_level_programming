606d5d1b480ae187cf37d3dd043e78d0e225677f
seugnou <40939034+seugnou@users.noreply.github.com>
2022-04-08 05:06:02 +0100

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, mull;
+	int num1, num2, mul;
 
 	if (argc != 3)
 	{
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		result = num1 * num2;
-		printf("%d\n", mull);
+		printf("%d\n", mul);
 		return (0);
 	}
 }
